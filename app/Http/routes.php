0da6c4de0fb8fe725258172d99ca826ce89e183e
Cristhian tombe <cristhiantombe@gmail.com>
2016-05-31 19:06:05 +0000

    plantilla adminLTE

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,9 +16,23 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'],function () {
+    
+    Route::get('/',function () {
+        echo 'Panel del administrador';
+    });
+    
     Route::resource('users','UsersController');
     Route::resource('estudiantes','EstudiantesController');
     Route::resource('profesores','ProfesoresController');
     Route::resource('carreras','CarrerasController');
     Route::resource('materias','MateriasController');
+});
+
+Route::group(['prefix' => 'estudiante'], function () {
+    
+    Route::get('/', function () {
+        echo 'Panel del estudiante';
+    });
+    
+    // Reoute::resource('materias_estudiante','materias_estudianteController');
 });
---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<!--
+Landing page based on Pratt: http://blacktie.co/demo/pratt/
+-->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }} ">
+    <meta name="author" content="Sergi Tur Badenas - acacha.org">
+
+    <meta property="og:title" content="Adminlte-laravel" />
+    <meta property="og:type" content="website" />
+    <meta property="og:description" content="Adminlte-laravel - {{ trans('adminlte_lang::message.landingdescription') }}" />
+    <meta property="og:url" content="http://demo.adminlte.acacha.org/" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE.png" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x600.png" />
+    <meta property="og:image" content="http://demo.adminlte.acacha.org/img/AcachaAdminLTE600x314.png" />
+    <meta property="og:sitename" content="demo.adminlte.acacha.org" />
+    <meta property="og:url" content="http://demo.adminlte.acacha.org" />
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@acachawiki" />
+    <meta name="twitter:creator" content="@acacha1" />
+
+    <title>{{ trans('adminlte_lang::message.landingdescriptionpratt') }}</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="{{ asset('/css/bootstrap.css') }}" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700' rel='stylesheet' type='text/css'>
+
+    <script src="{{ asset('/plugins/jQuery/jQuery-2.1.4.min.js') }}"></script>
+    <script src="{{ asset('/js/smoothscroll.js') }}"></script>
+
+
+</head>
+
+<body data-spy="scroll" data-offset="0" data-target="#navigation">
+
+<!-- Fixed navbar -->
+<div id="navigation" class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><b>GM</b></a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Ingresar</a></li>
+                    <li><a href="{{ url('/register') }}">Registrarse</a></li>
+                @else
+                    <li><a href="/home">{{ Auth::user()->name }}</a></li>
+                @endif
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</div>
+
+
+<section id="home" name="home"></section>
+<div id="headerwrap">
+    <div class="container">
+        <div class="row centered">
+            <div class="col-lg-12">
+                <h1>Gestor de materias universitarias</h1>
+                <h3>Aplicativo de software para la gestion de carreras, materias, profesores, estudiantes y su programacion de materias</h3>
+            </div>
+            <div class="col-lg-2">
+
+            </div>
+            <div class="col-lg-8">
+                <img class="img-responsive" src="{{ asset('/img/app-bg.png') }}" alt="">
+            </div>
+            <div class="col-lg-2">
+        
+            </div>
+        </div>
+    </div> <!--/ .container -->
+</div><!--/ #headerwrap -->
+
+<section id="desc" name="desc"></section>
+
+
+<div id="c">
+    <div class="container">
+        <p>
+            Software desarrollado con el lenguaje de programacion PHP (laravel 5.2)
+            Por el aprendiz <a href='https://github.com/cristom-co'>Cristhian Tombe</a> perteneciente al ADSI 88
+        </p>
+    </div>
+</div>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
+</body>
+</html>
